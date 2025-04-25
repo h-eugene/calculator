6453b9b6a97965c990c21fdf13e0575d470e8c3a
Eugene <134902746+H-Eugene@users.noreply.github.com>
2025-04-25 16:22:03 +0300

    Create UI of calculator

---
+++ logic.js
@@ -0,0 +1,31 @@
+function add(a , b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(op, a, b) {
+  switch (op) {
+    case "+": return add(a,b);
+    case "-": return subtract(a,b);
+    case "*": return multiply(a,b);
+    case "/": return divide(a,b);
+    default:
+      return NaN;
+  }
+}
+
+function createButtons() {
+  let btn = document.createElement("div");
+  
+}
